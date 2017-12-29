0bb5f5e84b47c6a14d0491bb72f5f77a2313682c
이용진 <Lambert.YJLee@gmail.com>
2017-12-29 19:42:07 +0900

    Initial Commit

---
+++ firstprec/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firstprec
+//
+//  Created by LeeYongJin on 2017. 12. 29..
+//  Copyright © 2017년 vian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
